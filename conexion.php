d9d640f870334c4c069e169dcf1df0f2bd3bc786
SiluLarp <68857830+SiluLarp@users.noreply.github.com>
2020-07-28 09:47:15 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 	//conexion a base de datos
-		$servidor = "localhost";
-		$usuario = "root";
-		$pass = "";
-		$nombreBD = "market_bd";
+		$servidor = "bbhjlrzzsm74vrwmugf0-mysql.services.clever-cloud.com";
+		$usuario = "urcwshiksnrsggn7";
+		$pass = "DtXAFrnqbX4S7GOcm7w0";
+		$nombreBD = "bbhjlrzzsm74vrwmugf0";
 
 		$conexion = mysqli_connect($servidor, $usuario, $pass, $nombreBD)
 					or die("Ocurrio un error con la conexion a la base de datos.");
