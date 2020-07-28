027480bfc0106ed5d867f447f5b8926bf86fa8fe
SiluLarp <68857830+SiluLarp@users.noreply.github.com>
2020-07-28 10:03:11 -0600

    Create index.php

---
+++ .github/workflows/index.php
@@ -0,0 +1,11 @@
+<?php
+	//conexion a base de datos
+		$servidor = "bbhjlrzzsm74vrwmugf0-mysql.services.clever-cloud.com";
+		$usuario = "urcwshiksnrsggn7";
+		$pass = "DtXAFrnqbX4S7GOcm7w0";
+		$nombreBD = "bbhjlrzzsm74vrwmugf0";
+
+		$conexion = mysqli_connect($servidor, $usuario, $pass, $nombreBD)
+					or die("Ocurrio un error con la conexion a la base de datos.");
+echo "probando BD";
+?>
